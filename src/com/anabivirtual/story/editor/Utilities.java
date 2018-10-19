0590004d8c22b005bf22afb3739897fb74a468de
Pedro Mariano <mariano.pedro@gmail.com>
2018-10-19 11:21:23 +0100

    Format rules.

--- src/com/anabivirtual/story/editor/Utilities.java
+++ src/com/anabivirtual/story/editor/Utilities.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.anabivirtual.story.editor;
 
 import java.util.ResourceBundle;
@@ -12,7 +8,7 @@ import java.util.ResourceBundle;
  */
 public class Utilities
 {
-	public static final ResourceBundle STRING_BUNDLE = ResourceBundle.getBundle("com/anabivirtual/story/editor/StringBundle");
+	public static final ResourceBundle STRING_BUNDLE = ResourceBundle.getBundle ("com/anabivirtual/story/editor/StringBundle");
 	public static String getString (String key)
 	{
 		return STRING_BUNDLE.getString (key);
