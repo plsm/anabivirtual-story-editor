6802719dca609b8e1752f3c17da99f4e34778021
Pedro Mariano <mariano.pedro@gmail.com>
2018-11-05 10:31:26 +0000

    Implemented methods to check and covert to a valid android resource filename.

--- src/com/anabivirtual/story/editor/Utilities.java
+++ src/com/anabivirtual/story/editor/Utilities.java
@@ -13,4 +13,62 @@ public class Utilities
 	{
 		return STRING_BUNDLE.getString (key);
 	}
+	/**
+	 * Checks if the given filename is a valid android resource.
+	 *
+	 * A resource filename must consist of the lowercase letters {@code a}
+	 * through {@code z}, the digits {@code 0} through {@code 9} and the
+	 * underscore character, {@code _}.
+	 *
+	 * @param filename the filename to check.
+	 * @return {@code true} if the string represents a valid android resource
+	 * filename.
+	 */
+	public static boolean isValidAndroidResourceFilename (String filename)
+	{
+		int last = filename.lastIndexOf ('.');
+		if (last == -1) {
+			last = filename.length ();
+		}
+		for (int i = 0; i < last; i++) {
+			char c = filename.charAt (i);
+			if (!Character.isDigit (c) && c != '_' && (c > 'z' || c < 'a')) {
+				return false;
+			}
+		}
+		return true;
+	}
+	/**
+	 * Convert a filename to a valid android resource filename.
+	 *
+	 * A resource filename must consist of the lowercase letters {@code a}
+	 * through {@code z}, the digits {@code 0} through {@code 9} and the
+	 * underscore character, {@code _}.
+	 *
+	 * @param filename the filename to convert.
+	 * @return a valid android resource filename.
+	 */
+	public static String convertToValidAndroidResourceFilename (String filename)
+	{
+		java.lang.StringBuilder result = new StringBuilder (filename.length ());
+		int last = filename.lastIndexOf ('.');
+		if (last == -1) {
+			last = filename.length ();
+		}
+		// process the characters up to the filename extension
+		for (int i = 0; i < last; i++) {
+			char c = filename.charAt (i);
+			if (Character.isDigit (c) || c == '_' || (c >= 'a' && c <= 'z')) {
+				result.append (c);
+			}
+			else if (c >= 'A' && c <= 'Z') {
+				result.append (Character.toLowerCase (c));
+			}
+		}
+		// add the filename extension
+		for (int i = last; i < filename.length (); i++) {
+			result.append (filename.charAt (i));
+		}
+		return result.toString ();
+	}
 }
