16702999893a644153669b4607adb53001b37f30
Pedro Mariano <mariano.pedro@gmail.com>
2018-11-06 12:26:44 +0000

    Test database has real locations and valid filenames.

--- src/com/anabivirtual/story/db/sql/test-database.sql
+++ src/com/anabivirtual/story/db/sql/test-database.sql
@@ -3,11 +3,12 @@
  * Created: 16/out/2018
  */
 
-INSERT INTO location (latitude, longitude, name) VALUES (60.2, 56.3, 'Igreja dos Apanhados');
-INSERT INTO location (latitude, longitude, name) VALUES (59.98, 57.9, 'Cais dos Destemidos');
+INSERT INTO location (latitude, longitude, name) VALUES (38.707911, -9.137939, 'Casa da Índia');
+INSERT INTO location (latitude, longitude, name) VALUES (38.708386, -9.135485, 'Alfândega');
+INSERT INTO location (latitude, longitude, name) VALUES (38.709236, -9.134746, 'Praça dos Escravos');
 
 INSERT INTO story (title, type, location_id) VALUES ('Alfredo Zangão', 1, 1);
 INSERT INTO story (title, type, location_id) VALUES ('Capitão Teutão', 1, 2);
 
-INSERT INTO audio (filename, story_id) VALUES ('alfredo-zangao.mp3', 1);
-INSERT INTO audio (filename, story_id) VALUES ('capitao-teutao.mp3', 2);
+INSERT INTO audio (filename, story_id) VALUES ('alfredo_zangao.mp3', 1);
+INSERT INTO audio (filename, story_id) VALUES ('capitao_teutao.mp3', 2);
