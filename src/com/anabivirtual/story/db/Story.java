6adadf69f844ddd77c32b54651be8d95244af4ef
Pedro Mariano <mariano.pedro@gmail.com>
2018-11-06 12:25:58 +0000

    Missing Story class.

---
+++ src/com/anabivirtual/story/db/Story.java
@@ -0,0 +1,15 @@
+package com.anabivirtual.story.db;
+
+/**
+ *
+ * @author pedro
+ * @date 6/nov/2018
+ */
+public class Story
+  extends AbstractStory
+{
+	public Story (long ID, Location location, String title)
+	{
+		super (ID, location, title);
+	}
+}
