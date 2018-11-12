5301f1dced6e6666355c1dfdbf27d25f5bcbdb04
Pedro Mariano <mariano.pedro@gmail.com>
2018-11-12 17:17:39 +0000

    Added more sample INSERTs.

--- src/com/anabivirtual/story/db/sql/test-database.sql
+++ src/com/anabivirtual/story/db/sql/test-database.sql
@@ -7,8 +7,15 @@ INSERT INTO location (latitude, longitude, name) VALUES (38.707911, -9.137939, '
 INSERT INTO location (latitude, longitude, name) VALUES (38.708386, -9.135485, 'Alfândega');
 INSERT INTO location (latitude, longitude, name) VALUES (38.709236, -9.134746, 'Praça dos Escravos');
 
-INSERT INTO story (title, type, location_id) VALUES ('Alfredo Zangão', 1, 1);
-INSERT INTO story (title, type, location_id) VALUES ('Capitão Teutão', 1, 2);
+INSERT INTO story (title, location_id) VALUES ('Alfredo Zangão', 1);
+INSERT INTO story (title, location_id) VALUES ('Capitão Teutão', 2);
+INSERT INTO story (title, location_id) VALUES ('El-Rei Manoel', 3);
 
 INSERT INTO audio (filename, story_id) VALUES ('alfredo_zangao.mp3', 1);
 INSERT INTO audio (filename, story_id) VALUES ('capitao_teutao.mp3', 2);
+
+INSERT INTO audio_book (filename, transcription, story_id) VALUES ('el_rei_manoel.mp3', 'El-Rei Manoel queria estar perto das suas mercadorias', 3);
+
+INSERT INTO historical_image (filename, story_id) VALUES ('retrato_el_rei_manoel.png', 3);
+
+INSERT INTO background_music (filename, region_center_latitude, region_center_longitude, region_radius) VALUES ('musica_quinhentista.mp3', 38.707911, -9.137939, 10);
