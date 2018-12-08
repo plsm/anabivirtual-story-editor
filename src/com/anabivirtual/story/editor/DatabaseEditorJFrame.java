ea1570c4f86762c3a4752d16adcae28ed60611ec
Pedro Mariano <mariano.pedro@gmail.com>
2018-12-08 11:40:49 +0000

    Closing the window only closes the database.

--- src/com/anabivirtual/story/editor/DatabaseEditorJFrame.java
+++ src/com/anabivirtual/story/editor/DatabaseEditorJFrame.java
@@ -259,7 +259,14 @@ public class DatabaseEditorJFrame
       deleteBackgroundMusicButton = new javax.swing.JButton();
       mapPanel = new javax.swing.JPanel();
 
-      setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+      setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+      addWindowListener(new java.awt.event.WindowAdapter()
+      {
+         public void windowClosed(java.awt.event.WindowEvent evt)
+         {
+            formWindowClosed(evt);
+         }
+      });
 
       locationsPanel.setLayout(new java.awt.BorderLayout());
 
@@ -480,6 +487,11 @@ public class DatabaseEditorJFrame
 			this.backgroundMusicTableModel.fireTableRowsDeleted (row, row);
 		}
    }//GEN-LAST:event_deleteBackgroundMusicButtonActionPerformed
+
+   private void formWindowClosed(java.awt.event.WindowEvent evt)//GEN-FIRST:event_formWindowClosed
+   {//GEN-HEADEREND:event_formWindowClosed
+		this.database.close ();
+   }//GEN-LAST:event_formWindowClosed
 
 	/**
 	 * Get a location to be used in inserted stories and places.
